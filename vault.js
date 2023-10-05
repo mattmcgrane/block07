f3903ad954d669d46cab322275cb19c828f9451e
Matthew <rew2300@hotmail.com>
2023-10-05 10:57:44 -0700

    second commit

---
+++ vault.js
@@ -0,0 +1,11 @@
+//Create vault code variables with calculations
+//10-40-39
+//create pop up with You have recieved this message because you have been chosen to open an important vault. Here is th secret combination: 
+
+//this step is setting up my calculations using the const tag since the combination will always remain the same
+const firstCombo= 5 + 5
+const secondCombo= 120 / 3
+const thirdCombo= 3 * 13
+
+//this step creates a popup window that will alert the user to the message with the combo
+alert("You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: " + firstCombo + "-"+ secondCombo + "-"+ thirdCombo)
